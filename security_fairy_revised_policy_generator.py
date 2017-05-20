00801a73860ada6786228ae4dc2e6d7028065e62
Justin Iravani <justin.iravani@1strategy.com>
2017-05-20 00:23:46 -0600

    too many commits

---
+++ security_fairy_revised_policy_generator.py
@@ -0,0 +1,63 @@
+import boto3
+import time
+from botocore.exceptions import ClientError
+
+# Create AWS session
+try:
+    session = boto3.session.Session(profile_name='training')
+except Exception as e:
+    session = boto3.session.Session()
+
+# Connect to Athena
+
+# Requested permissions
+# Create New Policy
+# Get existing permissions
+# Generate a diff
+# DynamoToken (QueryId)
+# Push to DynamoDB
+# Pass token
+
+#for testing Athena Query Id:
+# query_execution_id = 52e009f3-5a3a-4226-b562-dc9316a2995d
+
+def lambda_handler(event, context):
+    query_execution_id = event.get('query_execution_id')
+
+    if query_execution_id is None:
+        raise("Lambda Function requires 'query_execution_id' to execute.")
+
+    raw_query_result = get_query_results(query_execution_id)
+
+
+def get_query_results(query_execution_id):
+
+    athena_client = session.client('athena', region_name = 'us-east-1')
+
+    result_set = []
+
+    try:
+        results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+        for result in results["ResultSet"]["Rows"][1:]:
+            result_set.append(result["Data"])
+    except ClientError as e:
+        raise(e)
+
+    print(result_set)
+    return result_set
+
+def extract_service_level_permissions(results):
+    pass
+
+def build_revised_policy(entity_arn):
+    iam_client = session.client('iam')
+
+
+def build_revised_policy():
+    pass
+
+
+if __name__ == '__main__':
+    lambda_handler({
+        'query_execution_id': '52e009f3-5a3a-4226-b562-dc9316a2995d'
+    },{})
