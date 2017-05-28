26cd9351e051fdceb59e5234f3b6ba2f52fa829b
Justin Iravani <justin.iravani@1strategy.com>
2017-05-28 16:28:22 -0600

    Added entity arn regex

--- security_fairy_api_endpoint.py
+++ security_fairy_api_endpoint.py
@@ -1,19 +1,23 @@
 import boto3
 import json
 import re
-
+import os
 
 def lambda_handler(event, context):
 
+    # Default API Response returns an error
     api_return_payload = {
         'statusCode': 500,
-        'headers':{'Content-Type':'application/json'},
+        'headers':{
+            'Content-Type':'application/json'
+        },
         'body':'Interal Server Error'
     }
 
     try:
 
-        validate_inputs(event)
+        inputs = validate_inputs(event)
+        invoke_state_machine(inputs)
         api_return_payload['body'] = 'Inputs are valid.'
         api_return_payload['statusCode'] = 200
 
@@ -23,6 +27,14 @@ def lambda_handler(event, context):
 
     print(api_return_payload)
     return api_return_payload
+
+
+def invoke_state_machine(inputs):
+    print(json.dumps(inputs))
+    sfn_client.start_execution( stateMachineArn=os.environ['state_machine'],
+                                input=json.dumps(inputs)
+
+
 
 def validate_inputs(event):
 
@@ -35,11 +47,19 @@ def validate_inputs(event):
 
     entity_arn = input_payload.get('entity_arn')
 
-    pattern = re.compile("arn:aws:([a-zA-Z0-9\-])+:([a-z]{2}-[a-z]+-\d{1})?:(\d{12})?:(.*)")
+    pattern = re.compile("arn:aws:(sts|iam)+::(\d{12})?:(role|assumed-role)\/(.*)")
+
+    # Roles are valid: arn:aws:iam::842337631775:role/1S-Admins
+    #                  arn:aws:sts::281782457076:assumed-role/1S-Admins/alex
+    # Users are invalid: arn:aws:iam::842337631775:user/aaron
+
     if not pattern.match(entity_arn):
         raise ValueError('Invalid Resource ARN.')
 
-    print(entity_arn)
+    return {
+        'num_days': num_days,
+        'entity_arn': entity_arn
+    }
 
 
 
