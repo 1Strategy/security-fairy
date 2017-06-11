a2d5d784773d57e2647da42c1b1a71b8fabec475
Justin Iravani <justin.iravani@1strategy.com>
2017-06-10 22:55:27 -0600

    parameterized s3 buckets

--- security_fairy_athena_query.py
+++ security_fairy_athena_query.py
@@ -5,30 +5,18 @@ from botocore.exceptions import ClientError
 
 # Create AWS session
 try:
-    session = boto3.session.Session(profile_name='sandbox')
+    session = boto3.session.Session(profile_name='training')
 except Exception as e:
     session = boto3.session.Session()
 
-# Connect to Athena
-athena = session.client('athena', region_name='us-east-1')
-
-# Get named query
-queries = athena.list_named_queries()
-
-# Query Configurations
-config = {
-    'OutputLocation': 's3://security-fairy',
-    'EncryptionConfiguration': {
-        'EncryptionOption': 'SSE_S3'
-    }
-}
-
 
 def lambda_handler(event, context):
-    return execute_query(event.get('entity_arn'), event.get('num_days'))
+
+    event['execution_id']=execute_query(event['entity_arn'], event['num_days'], event['s3_bucket'])
+    return event
 
 
-def execute_query(entity_arn, num_days):
+def execute_query(entity_arn, num_days, s3_bucket):
 
     escaped_arn = build_escaped_arn(entity_arn)
 
@@ -44,17 +32,25 @@ def execute_query(entity_arn, num_days):
             , eventsource
           """.format(num_days=num_days, escaped_arn=escaped_arn)
     print(hql)
-    # Execute Query
-    execution = athena.start_query_execution(QueryString=hql,
-                                             ResultConfiguration=config)
 
-    print("Okay, I've submitted your query. Here is the query ID:")
-    print(execution['QueryExecutionId'])
-    print("Please wait while your results are generated.")
+    output = 's3://{s3_bucket}/tables'.format(s3_bucket=s3_bucket)
 
-    return {
-        'id': execution['QueryExecutionId']
+    config = {
+        'OutputLocation': output,
+        'EncryptionConfiguration': {
+            'EncryptionOption': 'SSE_S3'
+        }
     }
+
+    athena_client = session.client('athena')
+    execution = athena_client.start_query_execution(QueryString=hql,
+                                                    ResultConfiguration=config)
+
+    print("Query ID:")
+    print(execution['QueryExecutionId'])
+
+    return execution['QueryExecutionId']
+
 
 
 def build_escaped_arn(entity_arn):
@@ -68,7 +64,7 @@ if __name__ == '__main__':
     lambda_handler(
         {
             "entity_arn": "arn:aws:iam::281782457076:assumed-role/1s_tear_down_role",
-            "num_days": "-7"
+            "num_days": "-30"
         },
         {}
     )
