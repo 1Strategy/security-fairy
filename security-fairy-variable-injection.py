0c7848df5bd89e6b7c0aaedbf653db3bffae3f36
Justin Iravani <justin.iravani@1strategy.com>
2017-06-11 18:11:41 -0600

    Function to inject appropriate environment variables

---
+++ security-fairy-variable-injection.py
@@ -0,0 +1,16 @@
+import os
+import boto3
+
+def lambda_handler(event, context):
+
+    name    = os.environ['AWS_LAMBDA_FUNCTION_NAME']
+    region  = os.environ['AWS_REGION']
+    version = os.environ['AWS_LAMBDA_FUNCTION_VERSION']
+
+    lambda_client   = boto3.client('lambda', region_name=region)
+    raw_env_vars    = lambda_client.get_function(FunctionName=name, Qualifier=version)['Configuration']['Environment']['Variables']
+
+    for key, value in raw_env_vars.items():
+        event[key]=value
+
+    return event
