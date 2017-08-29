8d8832c70f7f9791fe677b5080f7939a24158544
Justin Iravani <justin.iravani@1strategy.com>
2017-08-29 09:39:57 -0600

    Tests, how do they work

--- test_classes.py
+++ test_classes.py
@@ -1,24 +1,33 @@
 import logging
+import pytest
+import json
 from security_fairy_tools import IAMPolicy
 from security_fairy_tools import IAMStatement
 from security_fairy_tools import Arn
 
 logging_level = logging.INFO
-statement = IAMStatement('Allow',["pot:atosoup","goat:cheese"],'*', logging_level = logging_level)
-statement.get_statement()
-policy = IAMPolicy(logging_level = logging_level)
-policy.add_statement(statement)
-print(policy.print_policy())
-print(policy.get_policy())
+# statement = IAMStatement('Allow',["pot:atosoup","goat:cheese"],'*', logging_level = logging_level)
+# statement.get_statement()
+# policy = IAMPolicy(logging_level = logging_level)
+# policy.add_statement(statement)
+# print(policy.print_policy())
+# print(policy.get_policy())
 
 # arn = Arn('arn:aws:iam::281782457076:role/1s_tear_down_role', logging_level = logging.DEBUG)
-arn = Arn('arn:aws:iam:us-east-1:842337631775:role/service-role/StatesExecutionRole-us-west-2')
-print("full_arn: "      + arn.get_full_arn())
-print("entity_name: "   + arn.get_entity_name())
-print("entity_type: "   + arn.get_entity_type())
-print("path: "          + arn.get_path())
-print("region: "        + arn.get_region())
-print("service: "       + arn.get_service())
-print("account_number: "+ arn.get_account_number())
-print(arn.is_role())
-print(arn.is_policy())
+# arn = Arn('arn:aws:iam:us-east-1:842337631775:role/service-role/StatesExecutionRole-us-west-2')
+# policy = IAMPolicy(logging_level = logging_level)
+# policy.add_action('lambda:Invoke')
+# policy.add_action('ec2:RunInstances')
+# policy.add_action('ec2:StartInstances')
+# policy.add_action('monitoring:CreateAlarm')
+# print(policy.print_policy())
+
+def test_iam_policy_class():
+    """Test Athena Query"""
+    policy = IAMPolicy(logging_level = logging_level)
+    policy.add_action('lambda:Invoke')
+    policy.add_action('ec2:RunInstances')
+    policy.add_action('ec2:StartInstances')
+    policy.add_action('monitoring:CreateAlarm')
+    print(policy.print_policy())
+    assert policy.get_policy() == {"Version": "2012-10-17", "Statement": [{"Action": ["ec2:RunInstances", "ec2:StartInstances"], "Resource": "*", "Effect": "Allow", "Sid": "SecurityFairyEc2Policy"}, {"Action": ["cloudwatch:CreateAlarm"], "Resource": "*", "Effect": "Allow", "Sid": "SecurityFairyCloudwatchPolicy"}, {"Action": ["lambda:Invoke"], "Resource": "*", "Effect": "Allow", "Sid": "SecurityFairyLambdaPolicy"}]}
