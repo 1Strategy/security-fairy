d3b8305997dc15605b251b4f0870d64fa0fa85fc
Justin Iravani <justin.iravani@1strategy.com>
2017-09-12 13:20:23 -0600

    Stubbing out code for dynamodb writes

--- security_fairy/denied_notification.py
+++ security_fairy/denied_notification.py
@@ -34,7 +34,7 @@ def lambda_handler(event, context):
     records = json.loads(gzfile.readlines()[0])['Records']
 
     access_denied_records = check_records_for_error_code(records)
-    # write_denied_actions_to_dynamodb(access_denied_records)
+    # write_denied_actions_to_dynamodb(access_denied_records, dynamodb_table)
     send_access_denied_notifications(access_denied_records, topic_arn)
 
 
@@ -56,6 +56,7 @@ def check_records_for_error_code(records, error_codes = ['AccessDenied', 'Access
             if not extracted_information in matched_error_records:
                 logging.debug('extracted_information doesn\'t already exist in list of access denieds')
                 matched_error_records.append(extracted_information)
+
     logging.debug(matched_error_records)
     return matched_error_records
 
@@ -64,23 +65,84 @@ def send_access_denied_notifications(access_denied_records, topic_arn):
 
     if access_denied_records:
         response = boto3.client('sns', region_name = 'us-east-1')\
-                        .publish(   TopicArn=topic_arn,
-                                    Message=json.dumps(access_denied_records),
-                                    Subject='Automated AWS Notification - Access Denied')
+                            .publish(   TopicArn=topic_arn,
+                                        Message=json.dumps(access_denied_records),
+                                        Subject='Automated AWS Notification - Access Denied')
 
 
-def write_denied_actions_to_dynamodb():
-    pass
+def write_denied_actions_to_dynamodb(access_denied_records, dynamodb_table):
+    #take in the below:
+    # [{"error_code": "AccessDenied", "arn": "arn:aws:sts::281782457076:assumed-role/serverless_api_gateway_step_functions/BackplaneAssumeRoleSession", "denied_action": "states:StartExecution"}, {"error_code": "AccessDenied", "arn": "arn:aws:sts::281782457076:assumed-role/serverless_api_gateway_step_functions/BackplaneAssumeRoleSession", "denied_action": "states:StartExecution"}]
+    # read the dynamodb_table, if the action already exists, do nothing
+    dynamodb_client = boto3.client('dynamodb', region_name = 'us-east-1')
+    response = dynamodb_client.scan(
+                            TableName=dynamodb_table,
+                            IndexName='entity_arn',
+                            AttributesToGet=[
+                                'execution_id',
+                            ],
+                            Select='ALL_ATTRIBUTES'|'ALL_PROJECTED_ATTRIBUTES'|'SPECIFIC_ATTRIBUTES'|'COUNT',
+                            ScanFilter={
+                                'string': {
+                                    'AttributeValueList': [
+                                        {
+                                            'S': 'string',
+                                            'N': 'string',
+                                            'B': b'bytes'
+                                        }
+                                    ],
+                                    'ComparisonOperator': 'EQ'|'NE'|'IN'|'LE'|'LT'|'GE'|'GT'|'BETWEEN'|'NOT_NULL'|'NULL'|'CONTAINS'|'NOT_CONTAINS'|'BEGINS_WITH'
+                                }
+                            }
+                        )
+
+
+    existing_denied_actions = dynamodb_client.get_item( TableName=dynamodb_table,
+                                                        Key={
+                                                                "execution_id": {
+                                                                    "S": execution_id
+                                                                }
+                                                            }
+                                                        )['Items']
+
+
+    for record in access_denied_records:
+        if is_access_denied_security_fairy_audited_role(record['arn']):
+            if not record['denied_action'] in existing_denied_actions:
+                pass
+                # dynamodb_client.update_item(TableName=dynamodb_table,
+                #                             Key={
+                #                                 "execution_id": {
+                #                                     "S": execution_id
+                #                                 }
+                #                             },
+                #                             AttributeUpdates={
+                #                                 "existing_policies": {
+                #                                     "Value":{"SS": existing_policies}
+                #                                 }})
 
 
 def is_access_denied_security_fairy_audited_role(role_arn):
+
+    iam_client  = boto3.client('iam')
     #Consumes an role arn and examines its attached policies to see
     #if they were created by security-fairy
-    role = Arn(role_arn)
-    role.get_entity_name()
-    #examines all attached policies and search for an attached policy with the
+    role        = Arn(role_arn)
+    role_name   = role.get_entity_name()
+
+    logging.debug(role_name)
+
+    attached_policies = iam_client.list_role_policies(RoleName=role_name)
+
+    # Examines all attached policies and search for an attached policy with the
     # following format:  *_security_fairy_revised_policy
-    # (see security_fairy_revised_policy_approve.py line 58)    
+    # (see security_fairy_revised_policy_approve.py line 58)
+
+    for policy in attached_policies:
+        logging.debug(policy)
+        if '_security_fairy_revised_policy' in policy:
+            return True
+
     return False
 
 
