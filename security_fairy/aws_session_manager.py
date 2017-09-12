fe1aeb78889e89ce0705f32db69d292d561dceed
Justin Iravani <justin.iravani@1strategy.com>
2017-09-12 11:46:28 -0600

    Formatting

--- security_fairy/aws_session_manager.py
+++ security_fairy/aws_session_manager.py
@@ -1,12 +1,14 @@
+import logging
+
 class AWS_Session:
 
     def __init__(region_name='us-east-1', profile_name='training'):
-
         self.session        = self.__create_new_session__()
         self.region_name    = region_name
         self.profile_name   = profile_name
 
     def __create_new_session__():
+        logging.debug("Creating a new boto3 Session object.")
         try:
             self.session = boto3.session.Session(   profile_name=self.profile_name,
                                                     region_name=self.region_name)
@@ -15,6 +17,7 @@ class AWS_Session:
 
     def get_session():
         if self.session.get_credentials().__is_expired__():
+            logging.debug("AWS Session expired.")
             self.__create_new_session__()
             return self.session
         return self.session
