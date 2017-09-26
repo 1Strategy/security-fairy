2fa589b35b1940057e2752cb0f252a95b340dc5d
Justin Iravani <justin.iravani@1strategy.com>
2017-09-25 20:11:37 -0600

    Included custom logger and refactored all functions to use AWSEntity

--- revert.py
+++ revert.py
@@ -4,11 +4,12 @@ import logging
 import os
 import re
 from tools import Arn
+from tools import AWSEntity
 from setup_logger import create_logger
 from botocore.exceptions import ProfileNotFound
 from boto3.dynamodb.conditions import Key
 
-logger = create_logger(name = __name__, logging_level=logging.INFO)
+logger = create_logger(name = "revert.py", logging_level=logging.INFO)
 
 try:
     SESSION = boto3.session.Session(profile_name='training',
@@ -29,8 +30,8 @@ def lambda_handler(event, context):
         logger.info('Request was an HTTP POST Request')
         posted_arn = json.loads(event['body'])['entity_arn']
         logger.info('Body: {}'.format(posted_arn))
-        entity_arn = Arn(posted_arn)
-        return post_response(entity_arn)
+        aws_entity = AWSEntity(posted_arn)
+        return post_response(aws_entity)
 
     return api_response()
 
@@ -42,33 +43,35 @@ def get_response():
 def get_all_iam_audited_entities():
 
     dynamodb_client = SESSION.client('dynamodb')
-    response_item = dynamodb_client.scan(
-                        TableName='security_fairy_dynamodb_table',
-                        AttributesToGet=[
-                            'entity_arn',
-                            'existing_policies'
-                        ])['Items']
+    response_item   = dynamodb_client.scan(   TableName='security_fairy_dynamodb_table',
+                                            AttributesToGet=[
+                                                'entity_arn',
+                                                'existing_policies'
+                                            ])['Items']
     logger.info(response_item)
     return response_item
 
 
-def post_response(entity_arn):
+def post_response(aws_entity):
     try:
-        revert_role_managed_policies(entity_arn)
+        revert_role_managed_policies(aws_entity)
     except Exception as e:
         # Generic "catch-all exception"
-        logger.info(e)
+        logger.error(e)
         return api_response(body='Error - Role wasn\'t reverted properly.')
 
     return api_response(statusCode=200, body='Success: The IAM Role has had it\'s pre-security fairy permissions established')
 
 
-def revert_role_managed_policies(role_arn):
+def revert_role_managed_policies(aws_entity):
     """
     Reverts role to pre-security fairy permissions
     """
-    associate_preexisting_policies(role_arn)
-    disassociate_security_fairy_policy(role_arn)
+    if not aws_entity.is_role():
+        raise ValueError("The submitted ARN must be for a role.")
+
+    associate_preexisting_policies(aws_entity)
+    disassociate_security_fairy_policy(aws_entity)
 
 
 def get_preexisting_policies(entity_arn):
@@ -78,13 +81,13 @@ def get_preexisting_policies(entity_arn):
     # reach out to security_fairy_dynamodb_table and get 'existing_policies' field
     response_item = dynamodb_client.scan(
                         TableName='security_fairy_dynamodb_table',
-                        IndexName='entity_arn',
-                        AttributesToGet=
-                        [
-                            'execution_id',
-                            'entity_arn',
-                            'existing_policies'
-                        ],
+                        # IndexName='entity_arn',
+                        # AttributesToGet=
+                        # [
+                        #     'execution_id',
+                        #     'entity_arn',
+                        #     'existing_policies'
+                        # ],
                         ScanFilter={
                             'entity_arn': {
                                 'AttributeValueList': [
@@ -104,14 +107,13 @@ def get_preexisting_policies(entity_arn):
     return existing_policies
 
 
-def associate_preexisting_policies(entity_arn):
+def associate_preexisting_policies(aws_entity):
 
     iam_client = SESSION.client('iam')
 
-    validated_entity = Arn(entity_arn)
-    existing_policies = get_preexisting_policies(entity_arn)
-    role_name = validated_entity.get_entity_name()
-
+    entity_arn          = aws_entity.get_full_arn()
+    existing_policies   = get_preexisting_policies(entity_arn)
+    role_name           = aws_entity.get_entity_name()
     # for each item in 'existing_policies' attach policy to 'role_arn'
     for policy in existing_policies:
         logger.info(policy)
@@ -119,11 +121,11 @@ def associate_preexisting_policies(entity_arn):
                                                             PolicyArn=policy)
 
 
-def disassociate_security_fairy_policy(entity_arn):
+def disassociate_security_fairy_policy(aws_entity):
     iam_client      = SESSION.client('iam')
-    arn = Arn(entity_arn)
-    account_number  = arn.get_account_number()
-    entity_name     = arn.get_entity_name()
+
+    account_number  = aws_entity.get_account_number()
+    entity_name     = aws_entity.get_entity_name()
 
     policy_arn      =  'arn:aws:iam::{account_number}:policy/security-fairy/{entity_name}-security-fairy-revised-policy'\
                             .format(account_number=account_number,
@@ -131,15 +133,17 @@ def disassociate_security_fairy_policy(entity_arn):
                                         .replace('_','-')
     logger.info(policy_arn)
 
-    detach_policy(entity_name, policy_arn)
-    delete_policy(policy_arn)
-
+    try:
+        detach_policy(entity_name, policy_arn)
+        delete_policy(policy_arn)
+    except iam_client.exceptions.NoSuchEntityException as error:
+         logging.info("Error deleting or detaching policy from role: {}, the entity doesn't exist.".format(error))
 
 def detach_policy(entity_name, policy_arn):
-    try:
-        iam_client.detach_role_policy(RoleName=entity_name, PolicyArn=policy_arn)
-    except Exception as error:
-        loggin.info(error)
+    iam_client      = SESSION.client('iam')
+    iam_client.detach_role_policy(RoleName=entity_name, PolicyArn=policy_arn)
+    logging.info("Detaching {} from {}".format(entity_name, policy_arn))
+
 
 
 def delete_policy(policy_arn):
@@ -186,5 +190,5 @@ if __name__ == '__main__':
     # associate_preexisting_policies("arn:aws:iam::281782457076:role/1s_tear_down_role")
     # get_all_iam_audited_entities()
     # print(nosql_to_list_of_dicts(get_all_iam_audited_entities()))
-    # event = {"resource":"/revert","path":"/revert","httpMethod":"POST","headers":None,"queryStringParameters":None,"pathParameters":None,"stageVariables":None,"requestContext":{"path":"/revert","accountId":"281782457076","resourceId":"l4irb8","stage":"test-invoke-stage","requestId":"test-invoke-request","identity":{"cognitoIdentityPoolId":None,"accountId":"281782457076","cognitoIdentityId":None,"caller":"AROAJFMSEYJPIQQGLKKO4:justiniravani","apiKey":"test-invoke-api-key","sourceIp":"test-invoke-source-ip","accessKey":"ASIAIGOR7BSSZ2T4WYUA","cognitoAuthenticationType":None,"cognitoAuthenticationProvider":None,"userArn":"arn:aws:sts::281782457076:assumed-role/1S-Admins/justiniravani","userAgent":"Apache-HttpClient/4.5.x (Java/1.8.0_131)","user":"AROAJFMSEYJPIQQGLKKO4:justiniravani"},"resourcePath":"/revert","httpMethod":"POST","apiId":"sloh70a4cj"},"body":"{\"entity_arn\":\"arn:aws:iam::281782457076:role/1s_tear_down_role\"}","isBase64Encoded":False}
-    # lambda_handler(event, {})
+    event = {"resource":"/revert","path":"/revert","httpMethod":"POST","headers":None,"queryStringParameters":None,"pathParameters":None,"stageVariables":None,"cognitoAuthenticationType":None,"cognitoAuthenticationProvider":None,"userArn":"arn:aws:sts::281782457076:assumed-role/1S-Admins/justiniravani","body":"{\"entity_arn\":\"arn:aws:iam::281782457076:role/1s_tear_down_role\"}","isBase64Encoded":False}
+    lambda_handler(event, {})
