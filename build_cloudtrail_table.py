4dc53b8f9b25e02290da7cfa649fc208d7ddb3ac
alex <alex@1strategy.com>
2017-12-28 11:13:07 -0700

    add cloudformation return values to build_cloudtrail_table.py

--- build_cloudtrail_table.py
+++ build_cloudtrail_table.py
@@ -23,7 +23,45 @@ AMZ_DATE          = TIME.strftime('%Y%m%dT%H%M%SZ')
 DATE_STAMP        = TIME.strftime('%Y%m%d')
 PROFILE           = 'sandbox'
 cloudtrail_bucket = os.environ("cloudtrail_bucket")
+SUCCESS = "SUCCESS"
+FAILED  = "FAILED"
 
+
+try:
+    from urllib2 import HTTPError, build_opener, HTTPHandler, Request
+except ImportError:
+    from urllib.error import HTTPError
+    from urllib.request import build_opener, HTTPHandler, Request
+
+
+def send(event, context, response_status, reason=None, response_data=None, physical_resource_id=None):
+    response_data = response_data or {}
+    response_body = json.dumps(
+        {
+            'Status': response_status,
+            'Reason': reason or "See the details in CloudWatch Log Stream: " + context.log_stream_name,
+            'PhysicalResourceId': physical_resource_id or context.log_stream_name,
+            'StackId': event['StackId'],
+            'RequestId': event['RequestId'],
+            'LogicalResourceId': event['LogicalResourceId'],
+            'Data': {'ConfigJson': response_data}
+        }
+    )
+
+    opener = build_opener(HTTPHandler)
+    request = Request(event['ResponseURL'], data=response_body)
+    request.add_header('Content-Type', '')
+    request.add_header('Content-Length', len(response_body))
+    request.get_method = lambda: 'PUT'
+    response = opener.open(request)
+    try:
+        response = opener.open(request)
+        print("Status code: {}".format(response.getcode()))
+        print("Status message: {}".format(response.msg))
+        return True
+    except HTTPError as exc:
+        print("Failed executing HTTP request: {}".format(exc.code))
+        return False
 
 def create_session():
     """Establish an AWS session through boto3
@@ -221,6 +259,8 @@ def lambda_handler(event, context):
         send(event, context, SUCCESS)
 
     except:
+        
+        return "Error"
 
         logging.info("Failed Execution")
         send(event, context, FAILED)
