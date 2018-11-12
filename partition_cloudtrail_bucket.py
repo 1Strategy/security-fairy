d1b42cfe233e7850bc3a52638104746b3c24ddf4
Alex <alex@1strategy.com>
2018-11-12 13:35:09 -0700

    include 'add partitions' query and submit query for execution

--- partition_cloudtrail_bucket.py
+++ partition_cloudtrail_bucket.py
@@ -10,9 +10,9 @@ See the AWS documentation for Athena here:
 import os
 import sys
 import json
-import datetime
 import logging
 import boto3
+from datetime import datetime
 from botocore.exceptions import ProfileNotFound
 
 
@@ -28,15 +28,40 @@ def lambda_handler(events, context):
     s3_bucket = os.environ['s3_bucket']
     athena_client = SESSION.client('athena')
 
-    output = 's3://{s3_bucket}/tables'.format(s3_bucket=s3_bucket)
+    output = f"s3://{s3_bucket}/security-fairy-partition-queries"
+    year = datetime.now().year
+    month = datetime.now().month
+    day = datetime.now().day
+    regions = ['us-west-2',
+                'us-west-1',
+                'us-east-2',
+                'us-east-1',
+                'ap-south-1',
+                'ap-northeast-2',
+                'ap-southeast-1',
+                'ap-southeast-2',
+                'ap-northeast-1',
+                'ca-central-1',
+                'cn-north-1',
+                'eu-central-1',
+                'eu-west-1',
+                'eu-west-2',
+                'eu-west-3',
+                'sa-east-1',
+                'us-gov-west-1'
+    ]
     config = {
         'OutputLocation': output,
         'EncryptionConfiguration': {
             'EncryptionOption': 'SSE_S3'
         }
     }
-    # ALTER TABLE cloudtrail_test ADD PARTITION (region='ap-northeast-2', year='2018', month='01', day='01')
-    response = athena_client.start_query_execution(
-        QueryString="create database if not exists logs;",
-        ResultConfiguration=config
-    )
+
+    for region in regions:
+        try:
+            response = athena_client.start_query_execution(
+                QueryString=f"ALTER TABLE cloudtrail ADD PARTITION (region={region}, year={year}, month={month}, day={day})",
+                ResultConfiguration=config
+            )
+            #change to logger
+            print(response)
